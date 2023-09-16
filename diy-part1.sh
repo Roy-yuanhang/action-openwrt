72c01dad043b6a91a3736824dc4dccf319a7df00
Lu Pengbo <67501523+Roy-yuanhang@users.noreply.github.com>
2023-09-16 21:33:18 +0800

    Update diy-part1.sh
    添加hello world

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,8 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# Add helloworld
+rm -rf package/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git -C package/helloworld pull
